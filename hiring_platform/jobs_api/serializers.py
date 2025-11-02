6623b7ed0101eedf498e402f80408a6a90ed0ead
Paras Ghimire <paras@example.com>
2025-11-02 10:45:00 +0545

    feat: add Job serializer

--- hiring_platform/jobs_api/serializers.py
+++ hiring_platform/jobs_api/serializers.py
@@ -10,3 +10,8 @@ class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = '__all__'
